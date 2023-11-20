381b6166b20b7b3ec5b8795a3f4bca6644f466ca
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-20 15:36:20 +0700

    menambahkan class getLokasiGunung

--- app/src/main/java/com/example/atapnegeri/MainActivity.kt
+++ app/src/main/java/com/example/atapnegeri/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.atapnegeri
 
+import android.app.Activity
 import android.graphics.Color
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Adapter
+import org.json.JSONObject
+import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
 
-    var modelMain : MutableList<ModelMain> = ArrayList()
+    var modelMain: MutableList<ModelMain> = ArrayList()
     lateinit var mainAdapter: MainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         }
-        if (Build.VERSION.SDK_INT >= 21){
+        if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
             window.statusBarColor = Color.TRANSPARENT
 
@@ -33,11 +37,61 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         mainAdapter = MainAdapter(this, modelMain)
-        rv,Lokasi.setLayoutManager(LinearLayoutManager(this))
+        rv, Lokasi.setLayoutManager(LinearLayoutManager(this))
         mainAdapter = MainAdapter(this, modelMain)
         rvLokasi.setLayoutManager(LinearLayoutManager(this))
         rvLokasi.setAdapter(mainAdapter)
         rvLokasi.setHasFixedSize(true)
 
+        getLokasiGunung()
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getLokasiGunung() {
+        try {
+            val stream = assets.open("nama_gunung.json")
+            val size = stream.available()
+            val buffer = ByteArray(size)
+            stream.read(buffer)
+            stream.close()
+            val strContent = String(buffer, StandardCharsets.UTF_8)
+            try {
+                val jsonObject = JSONObject(strContent)
+                val jsonArray = jsonObject.getJSONArray("gunung")
+                for (i in 0 until jsonArray.length()) {
+                    val dataApi = ModelMain()
+                    val jsonObjectData = jsonArray.getJSONObject(i)
+                    dataApi.strLokasi = jsonObjectData.getString("lokasi")
+                    modelMain.add(dataApi)
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        } catch (ignored: IOException) {
+            Toast.makeText(this@MainActivity,
+                "Oops, ada yang tidak beres. Coba ulangi beberapa saat lagi.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
+            val window = activity.window
+            val layoutParams = window.attributes
+            if (on) {
+                layoutParams.flags = layoutParams.flags or bits
+            } else {
+                layoutParams.flags = layoutParams.flags and bits.inv()
+            }
+            window.attributes = layoutParams
+        }
+    }
+
 }
+
