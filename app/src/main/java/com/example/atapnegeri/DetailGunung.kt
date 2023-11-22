53293425dec926b761bcf61f494941d83b8cd491
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-22 11:11:17 +0700

    menambahkan status bar

--- app/src/main/java/com/example/atapnegeri/DetailGunung.kt
+++ app/src/main/java/com/example/atapnegeri/DetailGunung.kt
@@ -1,8 +1,13 @@
 package com.example.atapnegeri
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.atapnegeri.ListGunung.Companion.setWindowFlag
 import com.google.android.gms.maps.OnMapReadyCallback
 
 class DetailGunung : AppCompatActivity(), OnMapReadyCallback {
@@ -19,6 +24,18 @@ class DetailGunung : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_gunung)
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
 
     }
 }
