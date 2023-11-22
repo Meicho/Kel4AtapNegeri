c3a44394b49e5239a507b1e29f327681a1e377e0
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-22 14:33:07 +0700

    menambahkan status bar pada kelas DetailPeralatan.kt

--- app/src/main/java/com/example/atapnegeri/DetailPeralatan.kt
+++ app/src/main/java/com/example/atapnegeri/DetailPeralatan.kt
@@ -1,10 +1,31 @@
 package com.example.atapnegeri
 
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.atapnegeri.ListGunung.Companion.setWindowFlag
 
 class DetailPeralatan : AppCompatActivity() {
 
     lateinit var modelPeralatan: ModelPeralatan
     var strNamaAlat: String? = null
     var strDeskripsi: String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_peralatan)
+
+        //set transparent statusbar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
 }
