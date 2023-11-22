586f9eecd0950815a86c37df25152645f1def92c
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-22 15:02:52 +0700

    menambahkan tampilan toolbar, intent, dan Companion object pada kelas  DetailTips.kt

--- app/src/main/java/com/example/atapnegeri/DetailTips.kt
+++ app/src/main/java/com/example/atapnegeri/DetailTips.kt
@@ -1,8 +1,10 @@
 package com.example.atapnegeri
 
+import android.app.Activity
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -26,5 +28,45 @@ class DetailTips : AppCompatActivity() {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
             window.statusBarColor = Color.TRANSPARENT
         }
+
+        setSupportActionBar(toolbar)
+        assert(supportActionBar != null)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        modelPeralatan = intent.getSerializableExtra(DETAIL_PERALATAN) as ModelPeralatan
+        if (modelPeralatan != null) {
+            strNamaAlat = modelPeralatan.strNamaPeralatan
+            strTips = modelPeralatan.strTipsPeralatan
+
+            Glide.with(this)
+                .load(modelPeralatan.strImagePeralatan)
+                .into(imageAlat)
+
+            tvNamaAlat.setText(strNamaAlat)
+            tvDetailAlat.setText(strTips)
+        }
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val DETAIL_PERALATAN = "DETAIL_PERALATAN"
+        fun setWindowFlag(activity: Activity, bits: Int, on: Boolean) {
+            val window = activity.window
+            val layoutParams = window.attributes
+            if (on) {
+                layoutParams.flags = layoutParams.flags or bits
+            } else {
+                layoutParams.flags = layoutParams.flags and bits.inv()
+            }
+            window.attributes = layoutParams
+        }
+    }
+
 }
