7026fbeb5713c3c67c556f8a2b48526a0f13be3a
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-22 14:57:34 +0700

    menambahkan tampilan status bar pada kelas  DetailTips.kt

--- app/src/main/java/com/example/atapnegeri/DetailTips.kt
+++ app/src/main/java/com/example/atapnegeri/DetailTips.kt
@@ -1,7 +1,12 @@
 package com.example.atapnegeri
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.example.atapnegeri.ListGunung.Companion.setWindowFlag
 
 class DetailTips : AppCompatActivity() {
     lateinit var modelPeralatan: ModelPeralatan
@@ -12,5 +17,14 @@ class DetailTips : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_peralatan)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
     }
 }
