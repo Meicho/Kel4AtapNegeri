795e7c8fe4dc7a3f58e8e1e68edfa4607c2b7694
rindkusumaa <rindakusumadewi23@gmail.com>
2023-11-27 12:44:20 +0700

    menambahkan adapter ListGunungAdapter.kt

--- app/src/main/java/Adapter/ListGunungAdapter.kt
+++ app/src/main/java/Adapter/ListGunungAdapter.kt
@@ -6,11 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.atapnegeri.R
 
-class ListGunungAdapter(private val context: Context, private val modelgunung:
-List<ModelGunung>) : RecyclerView.Adapter<ListGunungAdapter.ViewHolder() {
+class ListGunungAdapter(private val context: Context, private val modelGunung:
+List<ModelGunung>) : RecyclerView.Adapter<ListGunungAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_gunung, parent, false)
-        return RecyclerView.ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_gunung, parent, false)
+        return ViewHolder(view)
     }
 }
