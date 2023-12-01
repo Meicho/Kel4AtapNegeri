2ff1db5967b05b4f878898024ff87aa4d51fdc5e
rindkusumaa <rindakusumadewi23@gmail.com>
2023-12-01 09:52:37 +0700

    menambahkan fungsi override dan onBindViewHolder digunakan untuk menghubungkan data pada posisi tertentu dalam dataset dengan tampilan yang sesuai di ViewHolder. pengaturan untuk memuat gambar menggunakan Glide dan menetapkan listener klik pada setiap item untuk membuka detail peralatan ketika diklik.

--- app/src/main/java/Adapter/PeralatanAdapter.kt
+++ app/src/main/java/Adapter/PeralatanAdapter.kt
@@ -1,10 +1,12 @@
 package Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.atapnegeri.R
+import com.example.atapnegeri.activities.DetailPeralatan
 
 class PeralatanAdapter(private val context: Context, private val modelPeralatan:
 List<ModelPeralatan>) : RecyclerView.Adapter<PeralatanAdapter.ViewHolder>() {
@@ -16,5 +18,24 @@ List<ModelPeralatan>) : RecyclerView.Adapter<PeralatanAdapter.ViewHolder>() {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item_peralatan_tips, parent, false)
         return ListGunungAdapter.ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PeralatanAdapter.ViewHolder, position: Int) {
+        val data = modelPeralatan[position]
+
+       Glide.with(context)
+           .load(data.strImagePeralatan)
+           .load(holder.imagePeralatan)
+
+        holder.tvNamaAlat.text = data.strNamaPeralatan
+        holder.tvTipeAlat.text = data.strTipePeralatan
+
+        holder.cvListPeralatan.setOnClickListener {
+            val intent = Intent(context, DetailPeralatan::class.java)
+            intent.putExtra(DetailPeralatan.DETAIL_PERALATAN, modelPeralatan[position])
+            context.startActivity(intent)
+
+        }
+
     }
 }
