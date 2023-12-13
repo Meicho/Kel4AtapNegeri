59e20faa5f9709e5e5ddc2230b6f151818363233
rindkusumaa <rindakusumadewi23@gmail.com>
2023-12-13 17:40:27 +0700

    menambahkan fungsi pada MainAdapter.kt

--- app/src/main/java/Adapter/MainAdapter.kt
+++ app/src/main/java/Adapter/MainAdapter.kt
@@ -2,7 +2,11 @@ package Adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.atapnegeri.R
 
 class MainAdapter(private val context: Context?, private val modelMain: List<ModelMain>) :
 RecyclerView.Adapter<MainAdapter.ViewHolder>() {
@@ -12,6 +16,47 @@ RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_main, parent, false)
         return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val data = modelMain[position]
+
+        holder.tvLokasi.text = data.strLokasi
+
+        if (data.strLokasi == "Jawa Timur")
+            imageLokasiDrawable = R.drawable.ic_jatim
+        else if (data.strLokasi == "Jawa Tengah")
+            imageLokasiDrawable = R.drawable.ic_jateng
+        else if (data.strLokasi == "Jawa Barat")
+            imageLokasiDrawable = R.drawable.ic_jabar
+        else if (data.strLokasi == "Luar Pulau Jawa")
+            imageLokasiDrawable = R.drawable.ic_luar_jawa
+
+        holder.imagelokasi.setImageResource(imageLokasiDrawable)
+
+        holder.cvListLokasi.setOnClickListener {
+            val intent = Intent(context, ListGunung::class.java)
+            intent.putExtra(activity_list_gunung.LIST_GUNUNG, modelMain[position])
+            context.startActivity(intent)
+        }
+
+        override fun getItemCount(): Int {
+            return modelMain.size
+        }
+
+        class ViewHolder(item: View) : RecyclerView.ViewHolder(itemView) {
+            var cvListLokasi: CardView
+            var tvLokasi: TextView
+            var imageLokasi: ImageView
+
+            init {
+                cvListLokasi = itemView.cvListLokasi
+                tvLokasi = itemView.tvLokasi
+                imageLokasi = itemView.imageLokasi
+            }
+        }
+
+
     }
 
 
